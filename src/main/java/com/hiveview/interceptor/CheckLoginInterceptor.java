a73fcd80a7e9459a6f11406e65a06eaf367a3fe3
huxunqiang <huxunqiang@99114.com>
2017-05-15 18:53:28 +0800

    登录检查

--- src/main/java/com/hiveview/interceptor/CheckLoginInterceptor.java
+++ src/main/java/com/hiveview/interceptor/CheckLoginInterceptor.java
@@ -1,13 +1,12 @@
 package com.hiveview.interceptor;
 
 import com.hiveview.entity.Member;
+import com.hiveview.util.CheckLoginUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 
 public class CheckLoginInterceptor implements HandlerInterceptor{
@@ -17,7 +16,7 @@ public class CheckLoginInterceptor implements HandlerInterceptor{
 		@Override
 		public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws Exception {
 			String requestUrl = request.getServletPath();
-			if(!requestUrl.startsWith("/member"))
+			if(!CheckLoginUtil.isFilter(requestUrl))
 				return true;
 			Object usesession =request.getSession().getAttribute("currentUser");
 			if(usesession != null){
@@ -50,4 +49,5 @@ public class CheckLoginInterceptor implements HandlerInterceptor{
 			
 		}
 		public static String [] noInterceptor_url = {"toMember.html"};
+
 }
---
+++ src/main/java/com/hiveview/util/CheckLoginUtil.java
@@ -0,0 +1,48 @@
+package com.hiveview.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by hxq on 2017/5/15.
+ */
+public class CheckLoginUtil {
+
+    /**
+     * 需要过滤的 Url前缀
+     */
+    private static final List<String> filterUrlPrefix = Arrays.asList("member","myTest");
+
+    /**
+     * 是否需要登录过滤
+     * @param url
+     * @return
+     */
+    public static boolean isFilter(String url) {
+        return Optional.ofNullable(url).filter(u -> StringUtils.isNotBlank(u)).map(u1 ->{
+                    boolean flag = false;
+                    String tempUrl = StringUtils.removeStart(u1, "/");
+                    tempUrl=  StringUtils.substringBefore(tempUrl, "/");
+                    if (filterUrlPrefix.contains(tempUrl)) {
+                        flag = true;
+                    }
+                      return flag;
+                }
+        ).orElse(false);
+    }
+
+    public static void main(String[] args) {
+        System.err.println("1=============="+isFilter("/member/asd/dsa.html"));
+        System.err.println("2=============="+isFilter("/memberasd/dsa.html"));
+        System.err.println("3=============="+isFilter("/member"));
+        System.err.println("4=============="+isFilter("/membe"));
+        System.err.println("5=============="+isFilter("member"));
+        System.err.println("6=============="+isFilter("member/"));
+        System.err.println("7=============="+isFilter("/myTest/asd/dsa"));
+        System.err.println("8=============="+isFilter("/myTest1"));
+    }
+
+}
