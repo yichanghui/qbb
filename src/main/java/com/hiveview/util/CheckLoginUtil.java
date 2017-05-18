96bca41738469f362eb8d8ce447510e61230593f
huxunqiang <huxunqiang@99114.com>
2017-05-18 17:59:30 +0800

    登录检查

--- src/main/java/com/hiveview/util/CheckLoginUtil.java
+++ src/main/java/com/hiveview/util/CheckLoginUtil.java
@@ -14,7 +14,7 @@ public class CheckLoginUtil {
     /**
      * 需要过滤的 Url前缀
      */
-    private static final List<String> filterUrlPrefix = Arrays.asList("member","myTest");
+    private static final List<String> filterUrlPrefix = Arrays.asList("/member/","myTest");
 
     /**
      * 是否需要登录过滤
@@ -23,15 +23,15 @@ public class CheckLoginUtil {
      */
     public static boolean isFilter(String url) {
         return Optional.ofNullable(url).filter(u -> StringUtils.isNotBlank(u)).map(u1 ->{
-                    boolean flag = false;
-                    String tempUrl = StringUtils.removeStart(u1, "/");
-                    tempUrl=  StringUtils.substringBefore(tempUrl, "/");
-                    if (filterUrlPrefix.contains(tempUrl)) {
-                        flag = true;
-                    }
-                      return flag;
+//                    boolean flag = false;
+//                    String tempUrl = StringUtils.removeStart(u1, "/");
+//                    tempUrl=  StringUtils.substringBefore(tempUrl, "/");
+//                    if (filterUrlPrefix.contains(tempUrl)) {
+//                        flag = true;
+//                    }
+//                    return flag;
+                    return filterUrlPrefix.stream().anyMatch(u -> url.contains(u));
                 }
         ).orElse(false);
     }
-
 }
